756d80e8919ed9d5339af7922c07da263b1a47cd
nikunj bansal <nikunj.iitd@gmail.com>
2017-06-12 12:56:23 +0530

    added like functionality

--- web/js/blog.js
+++ web/js/blog.js
@@ -5,14 +5,7 @@
 var like = (function () {
     var isLiked = false;
     var toggle_like_blog = function (blog_id) {
-        $.ajax({
-            type: "POST",
-            url: '/blog/like',
-            data: {blogId: blog_id},
-            success: function (data) {
-                document.getElementById(blog_id).innerHTML = data.likes+' likes';
-            }
-        });
+
         this.isLiked = !this.isLiked;
     };
 
@@ -21,4 +14,26 @@ var like = (function () {
         toggle_like_blog: this.toggle_like_blog
     };
 
+})();
+
+var toggle_like_blog = (function () {
+    var userLike = {
+        Like : 'Unlike',
+        Unlike: 'Like'
+    };
+    var toggleLikeButton = function (blog_id) {
+        var likeSpan = $("#"+blog_id+"likeSpan span");
+        likeSpan.text(userLike[likeSpan.text()]);
+    };
+    return function (blog_id) {
+        $.ajax({
+            type: "POST",
+            url: '/blog/like',
+            data: {blogId: blog_id},
+            success: function (data) {
+                document.getElementById(blog_id).innerHTML = data.likes+' likes';
+                toggleLikeButton(blog_id);
+            }
+        });
+    }
 })();
